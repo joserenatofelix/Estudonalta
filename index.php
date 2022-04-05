618ace2568480e97aed8d19edfc474b56f917f36
José Renato Felix da Silva <93296913+joserenatofelix@users.noreply.github.com>
2022-04-05 14:30:02 -0300

    update

--- index.php
+++ index.php
@@ -4,13 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Título da Página</title>
+    <title>Listagem de Jogos</title>
     <link rel="stylesheet" href="estilo/estilo.css"/>
 </head>
 <body>
   <div id="corpo">
-      <h1>Olá!</h1>
+      <h1>Escolha seu Jogo</h1>
+      <table class="listagem">
+          <tr><td>Foto<td>Nome<td>Adm
+          <tr><td>Foto<td>Nome<td>Adm
+          <tr><td>Foto<td>Nome<td>Adm
+          <tr><td>Foto<td>Nome<td>Adm            
 
+      </table>
   </div>
 </body>
 </html>
